44c4cfe33b3a400a1119a34994459f5eb1cdcf76
Gisela Lopez <gisela.lopez@colegiomiralmonte.es>
2026-03-03 19:59:02 +0100

    update

--- producto.java
+++ producto.java
@@ -1,5 +1,8 @@
 public class producto {
     public static void main(String[] args) {
 
+        Productos producto = new Productos("Samsung s26", 999, 5);
+        Productos raton = new Productos("Razer V2 Pro", 120, 3);
+        Productos teclado = new Productos("MarsGaming", 230, 2);
     }
 }
