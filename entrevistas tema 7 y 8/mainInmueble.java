7160bfe864b95dd6087ea86ed0ccd66976c90e5e
Gisela Lopez <gisela.lopez@colegiomiralmonte.es>
2026-03-06 03:14:58 +0100

    update

--- entrevistas tema 7 y 8/mainInmueble.java
+++ entrevistas tema 7 y 8/mainInmueble.java
@@ -5,43 +5,12 @@ public class mainInmueble {
 
         Scanner sc = new Scanner(System.in);
 
-        // opcionPiso();
-        // opcionCasa();
-        // opcionLocal();
-        comprar(sc);
+        // mostrarPiso(sc);
+        // mostrarLocal(sc);
+        // mostrarCasa(sc);
+        // comprar(sc);
         // soyVendedor(sc);
         // pedirInfoPiso(sc);
-
-    }
-
-    private static void opcionPiso() {
-
-        Inmueble piso = new Piso(150, 2300, "Calle Turquesa, 23, Bajo Izquierda", "Alejandro Perez", "642080864",
-                false, 3, true);
-
-        piso.mostrarInformacion();
-        System.out.println("Su inmueble está valorado en: " + piso.calcularPrecioFinal() + " euros.");
-        piso.llamarDueno();
-
-    }
-
-    private static void opcionCasa() {
-
-        // ejemplo de casa con jardín y casa no adosada
-
-        Inmueble casa = new Casa(235, 3000, "Calle Amatista, 36", "Pepa Garcia", "684016573", true, true, 2, false);
-        casa.mostrarInformacion();
-        System.out.println("Su inmueble está valorado en: " + casa.calcularPrecioFinal() + " euros.");
-        casa.llamarDueno();
-    }
-
-    private static void opcionLocal() {
-
-        Inmueble local = new Local(80, 500, "Calle San Fernando, 45", "Jose Perez", "654326754", true, "Restaurante",
-                true);
-        local.mostrarInformacion();
-        System.out.println("Su inmueble está valorado en: " + local.calcularPrecioFinal() + " euros.");
-        local.llamarDueno();
 
     }
 
@@ -266,4 +235,46 @@ public class mainInmueble {
         return opcion;
     }
 
+    /*
+     * private static void opcionPiso() {
+     * 
+     * Inmueble piso = new Piso(150, 2300, "Calle Turquesa, 23, Bajo Izquierda",
+     * "Alejandro Perez", "642080864",
+     * false, 3, true);
+     * 
+     * piso.mostrarInformacion();
+     * System.out.println("Su inmueble está valorado en: " +
+     * piso.calcularPrecioFinal() + " euros.");
+     * piso.llamarDueno();
+     * 
+     * }
+     */
+
+    /*
+     * private static void opcionCasa() {
+     * 
+     * // ejemplo de casa con jardín y casa no adosada
+     * 
+     * Inmueble casa = new Casa(235, 3000, "Calle Amatista, 36", "Pepa Garcia",
+     * "684016573", true, true, 2, false);
+     * casa.mostrarInformacion();
+     * System.out.println("Su inmueble está valorado en: " +
+     * casa.calcularPrecioFinal() + " euros.");
+     * casa.llamarDueno();
+     * }
+     */
+
+    /*
+     * private static void opcionLocal() {
+     * 
+     * Inmueble local = new Local(80, 500, "Calle San Fernando, 45", "Jose Perez",
+     * "654326754", true, "Restaurante",
+     * true);
+     * local.mostrarInformacion();
+     * System.out.println("Su inmueble está valorado en: " +
+     * local.calcularPrecioFinal() + " euros.");
+     * local.llamarDueno();
+     * 
+     * } /*
+     */
 }
